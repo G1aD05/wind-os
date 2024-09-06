78f84da5b842949b23e0644a74d2e77922ef2b55
G1aD05 <ezrag2000@proton.me>
2024-09-05 19:49:12 -0700

    Rename time.py to apps/time.py

---
+++ apps/time.py
@@ -0,0 +1,2 @@
+import datetime as dt
+print(f" The time and date right now is {dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
--- time.py
+++
@@ -1,2 +0,0 @@
-import datetime as dt
-print(f" The time and date right now is {dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
