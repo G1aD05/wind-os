f3099c87d33ee0f6d177d10fafaba200bb5bf9d6
G1aD05 <ezrag2000@proton.me>
2024-09-07 12:13:25 -0700

    Update downloader.py

--- apps/downloader.py
+++ apps/downloader.py
@@ -1,3 +1,21 @@
 from urllib.request import urlretrieve
+import os
 
-urlretrieve(input('URL: '), f'{input("Output Dir: ")}{input("Output Name: ")}')
+
+def main():
+    output = input('Output path (Include file name ex: path/to/file/file_name.py): ')
+    url = input('URL: ')
+
+    # Ensure the directory exists
+    directory = os.path.dirname(output)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    try:
+        urlretrieve(url, output)
+        print(f"File downloaded and saved as {output}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+main()
